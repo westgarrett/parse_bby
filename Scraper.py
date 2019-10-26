6622e9bbcfc72a65e35bd0e5f741ec37cd2a4452
Garrett West <west.garrett@utexas.edu>
2019-10-26 12:20:06 -0500

    Added get_pages() and related methods

--- Scraper.py
+++ Scraper.py
@@ -21,11 +21,18 @@ class Scraper:
         contents = depends.BeautifulSoup(req.content, "html5lib")
         return contents
 
-    # Returns the quantities of each item at a set location. Must use selenium to open each item link.
+    # Recursively accesses each item page, accesses "Specifications" if present, and appends the "Quantity" field to a
+    # list. Returns the quantities of each item at a set location (future method?). Must use selenium to open each item
+    # link.
     def get_quantities(self):
         pass
 
-    # Returns a list of UPCs from the search results. Must use selenium to open each item.
+    # Returns the link to each product on the given page.
+    def get_product_links(self):
+        pass
+
+    # Recursively accesses each item page, accesses "Specifications" if present, and appends the "UPC" field to a list.
+    # Returns the list of UPCs from the search results. Must use selenium to open each item.
     def get_upcs(self):
         pass
 
@@ -47,7 +54,7 @@ class Scraper:
 
     # Returns the number of search results on the page. If more than 25, selenium must be used to list additional
     # results. For some reason, the best buy website sometimes returns an  (n - 1) result count. Not sure what the
-    # conditions are for that
+    # conditions are for that.
     def get_num_results(self):
         class Format:
             def __str__(self):
@@ -76,10 +83,10 @@ class Scraper:
     def get_num_pages(self):
         return len(self.get_pages())
 
-    # Returns the next page of results if the number of results is more than 25, or there are page links present
-    # at the end of the page due to the bug listed in the get_num_results() method notes.
+    # Returns the next page of results if the number of pages is more than 0
     def get_next_page(self):
-        if int(self.get_num_results()) > 25:
+        page_list = self.get_pages()
+        num_pages = len(page_list)
+        for i in range(num_pages):
             driver = depends.webdriver.Firefox()
             driver.get(self.url)
-            pages = driver.find_element_by_class_name("paging-list")
