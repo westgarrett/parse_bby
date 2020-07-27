0a1d7afdb31329a7ad68a4eb5c6ed2ab9099da64
westgarrett <west.garrett@utexas.edu>
2020-07-27 18:14:44 -0500

    Removed nonessential code and venv from git

--- CarrierScraper.py
+++ CarrierScraper.py
@@ -1,8 +1,20 @@
+import csv
 from Scraper import Scraper
+from Scraper import function_timer
+from bs4 import BeautifulSoup
+from regex import regex
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class CarrierScraper(Scraper):
 
-    def __init__(self, url=""):
-        super().__init__(url)
+    def __init__(self):
+        super().__init__()
+        self.scraper = Scraper()
         self.contents = self._get_contents()
 
+    ####################################################################################################################
+    # Functions for carrier phone pages                                                                                #
+    ####################################################################################################################
+
+    
